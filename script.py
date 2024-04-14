3b91fffec20118420cd0c6c0060ff725f4445161
PanRabbit <samuelnh012@gmail.com>
2024-04-14 21:57:15 +0100

    removed adapter from json dump, was causing errors

--- script.py
+++ script.py
@@ -4,8 +4,7 @@ import json
 from datetime import datetime
 from pathlib import Path
 
-
-myprompt="no data"
+myprompt = "no data"
 
 params = {
     "name": "AutoSave",
@@ -14,33 +13,39 @@ params = {
     "custom string": "n/a",
 }
 
-def save_data(string,timestamp=True):
-    mydate=datetime.now().strftime('%Y%m%d')
+
+def save_data(string, timestamp=True):
+    mydate = datetime.now().strftime('%Y%m%d')
     fname = f"{mydate}_text_log.txt"
-    
-    file_path=f'extensions/{params["name"]}/output'
-    
+
+    file_path = f'extensions/{params["name"]}/output'
+
     if not Path(file_path).exists():
         Path(file_path).mkdir()
-    
+
     model = shared.model_name
-    adapter = getattr(shared.model,'active_adapter','None')    
-        
+    adapter = "TEST"  # getattr(shared.model, 'active_adapter', 'None')
+
+    # with open(Path(f'{file_path}/{fname}'), 'a+', encoding='utf-8') as f:
+    #     f.write(json.dumps({"model": model, "adapter": adapter, "prompt": myprompt, "reply": string}, indent=2))
+
     with open(Path(f'{file_path}/{fname}'), 'a+', encoding='utf-8') as f:
-        f.write(json.dumps({"model": model, "adapter":  adapter, "prompt" : myprompt, "reply":string} , indent=2 ))
-    
+        f.write(json.dumps({"prompt": myprompt, "reply": string}, indent=2))
+
     return Path(f'{file_path}/{fname}')
+
 
 def input_modifier(string):
     """
     This function is applied to your text inputs before
     they are fed into the model.
-    """ 
+    """
     global myprompt
-    myprompt=string
-    #print (f"input query:{myprompt}")
+    myprompt = string
+    # print (f"input query:{myprompt}")
 
     return string
+
 
 def output_modifier(string):
     """
@@ -48,10 +53,11 @@ def output_modifier(string):
     """
     if not params['activate']:
         return string
-    
-    save_data(string,timestamp=False)
+
+    save_data(string, timestamp=False)
 
     return string
+
 
 def bot_prefix_modifier(string):
     """
@@ -61,12 +67,12 @@ def bot_prefix_modifier(string):
     """
     return string
 
+
 def ui():
     # Gradio elements
     activate = gr.Checkbox(value=params['activate'], label='Activate AutoSave')
-    #string = gr.Textbox(value=params["bias string"], label='Custom Text')
+    # string = gr.Textbox(value=params["bias string"], label='Custom Text')
 
     # Event functions to update the parameters in the backend
-    #string.change(lambda x: params.update({"custom string": x}), string, None)
+    # string.change(lambda x: params.update({"custom string": x}), string, None)
     activate.change(lambda x: params.update({"activate": x}), activate, None)
-
